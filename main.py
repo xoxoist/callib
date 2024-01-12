58c04d785cca988e83a6e8b1918bec53108a4da1
muhammadisa <msawjk@gmail.com>
2024-01-13 01:23:47 +0700

    Simple lib http caller

--- main.py
+++ main.py
@@ -3,10 +3,16 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import json
 import requests
+from typing import Type, List
 from enum import Enum, auto
+from pydantic import BaseModel
+from pydantic import ValidationError
+from requests import Response
 from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
+
 
 class Method(Enum):
     POST = auto()
@@ -17,6 +23,33 @@ class Method(Enum):
 
     def __str__(self):
         return self.name
+
+
+class Condition(Enum):
+    Malformed = auto()
+    Unregister = auto()
+    Timeout = auto()
+    Invalid = auto()
+
+    def __str__(self):
+        return self.name
+
+
+class Perform:
+    def __init__(self, name: str, path: str, method: Method, response: Type[BaseModel]):
+        self.response_model = response
+        self.path = path
+        self.method = method
+        self.name = name
+
+
+class PerformResult:
+    def __init__(self, response: Response, perform: Perform):
+        casted_result = perform.response_model(**json.loads(response.text))
+        self.data = casted_result.model_dump()
+        self.http_code = response.status_code
+        self.headers = response.headers
+
 
 class Call:
     def __init__(self, base_url: str, **kwargs):
@@ -31,6 +64,7 @@ class Call:
         self.session = requests.Session()
         self.session.mount("http://", self.adapter)
         self.session.mount("https://", self.adapter)
+        self.apis: List[Perform] = []
 
     def __create_http_adapter(self, connection: int, size: int, retry: int) -> HTTPAdapter:
         return HTTPAdapter(
@@ -44,20 +78,71 @@ class Call:
     def add_header(self, header: dict):
         self.session.headers.update(header)
 
-    def call(self, method: Method, path: str):
-        complete_url = f"{self.__base_url}{path}" if self.__base_url else path
-        return self.session.request(str(method), complete_url)
+    def add_api(self, perform: Perform):
+        self.apis.append(perform)
+
+    def execute(self, name: str, request: BaseModel) -> PerformResult | Condition:
+        try:
+            selected_api = [api for api in self.apis if api.name == name][0]
+            path = selected_api.path
+            method = str(selected_api.method)
+            complete_url = f"{self.__base_url}{path}" if self.__base_url else path
+            request_dumped = request.model_dump()
+            header_data = request_dumped["headers"]
+            if header_data:
+                self.session.headers.update(header_data)
+                del request_dumped["headers"]
+            result = self.session.request(method, complete_url, json=request_dumped)
+            return PerformResult(result, selected_api)
+        except IndexError:
+            return Condition.Unregister
+        except TimeoutError:
+            return Condition.Timeout
+        except json.JSONDecodeError:
+            return Condition.Malformed
+        except ValidationError:
+            return Condition.Invalid
+
+
+class CreatePostReq(BaseModel):
+    title: str | None
+    body: str | None
+    userId: int | None
+    headers: dict = {}
+
+
+class CreatePostRes(BaseModel):
+    id: int | None = None
+    title: str | None = None
+    body: str | None = None
+    userId: int | None = None
 
 
 def main():
-    cl_jsonplaceholder = Call("https://jsonplaceholder.typicode.com/", connection=5, size=10, retry=5)
+    # api initialization
+    base_url = "https://1290912671264cc1a9235d4525eef0b1.api.mockbin.io/"
+    cl_jsonplaceholder = Call(base_url, connection=5, size=10, retry=5)
     cl_jsonplaceholder.add_header({"Content-Type": "application/json"})
-    result = cl_jsonplaceholder.call(Method.GET, "posts")
-    print(result.status_code)
-    print(result.elapsed)
-    print(result.headers)
-    print(result.reason)
-    print(result.text)
+    cl_jsonplaceholder.add_api(Perform("create_post", "posts", Method.POST, CreatePostRes))
+
+    # api execution
+    create_post_request = CreatePostReq(
+        headers={"User-Agent": "Call-Lib-Test/1.0"},
+        title="Foo2",
+        body="Bar2",
+        userId=2,
+    )
+
+    # checking condition
+    create_post_res = cl_jsonplaceholder.execute("create_post", create_post_request)
+    if type(create_post_res) is not PerformResult:
+        print("error while calling api", create_post_res)
+    if type(create_post_res) is PerformResult:
+        created_post = CreatePostRes(**create_post_res.data)
+        print("id", created_post.id)
+        print("title", created_post.title)
+        print("body", created_post.body)
+        print("userId", created_post.userId)
 
 
 # Press the green button in the gutter to run the script.
